6b4c9b9419a74c2f5d8d1f8709684e7012407ce5
Sean Fioritto <sean@planningforaliens.com>
2025-01-16 11:29:46 -0600

    Builder pattern working better with generics

--- dsl/exploration.ts
+++ dsl/exploration.ts
@@ -17,42 +17,39 @@ type Simplify<T extends JsonObject> = {
     : never
   : never;
 
-interface StepBlock<Ctx extends JsonObject, Out extends JsonObject> {
+interface StepBlock<ContextIn extends JsonObject, ActionOut, ContextOut> {
   title: string;
-  action: ((context: Ctx) => Out | Promise<Out>);
-  reduce?: (result: Out, context: Ctx) => Simplify<Merge<Ctx, Out>>;
+  action: ((context: ContextIn) => ActionOut | Promise<ActionOut>);
+  reduce?: (result: ActionOut, context: ContextIn) => ContextOut
 }
 
-// I think this can work. What you are thinking here is you have to pass in action that returns a serializable output. But you want actions to be able to return non-serializable output. Also, you want to have a function signature with step where if it's a function, but not an action block, then it just assumes that that's a function that can return a serializable object and then it just assumes you're going to do a simple reducer, so you don't have to do that yourself. So I think in order to do that, I think you need to have the step accept action blocks instead of functions. I think the action blocks then have handlers with the T and the T output, et cetera. That way all the types work. I think it can work. I think if you can do that, then you can have type imprints and it'll actually, and the builder syntax is not really all that bad. It's actually basically the same. But you'll still have action functions and you'll still have reducer functions. It'll look really, really similar.
-
-export function createWorkflow<TContext extends JsonObject = {}>() {
-  function addSteps<T extends JsonObject>(steps: StepBlock<any, any>[]) {
+export function createWorkflow<InitialContext extends JsonObject = {}>() {
+  function addSteps<ContextIn extends JsonObject>(steps: StepBlock<any, any, any>[]) {
     return {
-      step<TOutput extends JsonObject>(
+      step<ActionOut, ContextOut extends JsonObject>(
         title: string,
-        action: (context: T) => TOutput | Promise<TOutput>,
-        reduce?: (result: TOutput, context: T) => Simplify<Merge<T, TOutput>>
+        action: (context: ContextIn) => ActionOut | Promise<ActionOut>,
+        reduce?: (result: ActionOut, context: ContextIn) => ContextOut
       ) {
-        const newStep: StepBlock<T, TOutput> = {
+        const newStep: StepBlock<ContextIn, ActionOut, ContextOut> = {
           title,
           action,
           reduce,
         };
         const newSteps = [...steps, newStep];
-        type NewContext = Simplify<Merge<T, TOutput>>;
-        return addSteps<NewContext>(newSteps);
+        return addSteps<ContextOut>(newSteps);
       },
       build(name: string) {
         return {
           name,
           steps,
-          async run(initialContext: Partial<T> = {}) {
-            let context = initialContext as T;
+          async run(initialContext: InitialContext) {
+            let context = initialContext;
             for (const step of steps) {
               const result = await step.action(context);
               context = step.reduce
                 ? step.reduce(result, context)
-                : { ...context, ...result };
+                : initialContext;
             }
             return context;
           }
@@ -61,13 +58,14 @@ export function createWorkflow<TContext extends JsonObject = {}>() {
     };
   }
 
-  return addSteps<TContext>([]);
+  return addSteps<InitialContext>([]);
 }
 
 const workflow = createWorkflow()
   .step(
     "Step 1",
-    () => ({ count: 1 })
+    () => ({ count: 1 }),
+    (result) => result,
   )
   .step(
     "Step 2",
