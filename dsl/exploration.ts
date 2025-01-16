5ebfa4b680f5cd849f0f387b1e785dd64603e0d1
Sean Fioritto <sean@planningforaliens.com>
2025-01-16 07:18:27 -0600

    note

--- dsl/exploration.ts
+++ dsl/exploration.ts
@@ -19,9 +19,11 @@ type Simplify<T extends JsonObject> = {
 
 interface StepBlock<Ctx extends JsonObject, Out extends JsonObject> {
   title: string;
-  action: (context: Ctx) => Out | Promise<Out>;
+  action: ((context: Ctx) => Out | Promise<Out>);
   reduce?: (result: Out, context: Ctx) => Simplify<Merge<Ctx, Out>>;
 }
+
+// I think this can work. What you are thinking here is you have to pass in action that returns a serializable output. But you want actions to be able to return non-serializable output. Also, you want to have a function signature with step where if it's a function, but not an action block, then it just assumes that that's a function that can return a serializable object and then it just assumes you're going to do a simple reducer, so you don't have to do that yourself. So I think in order to do that, I think you need to have the step accept action blocks instead of functions. I think the action blocks then have handlers with the T and the T output, et cetera. That way all the types work. I think it can work. I think if you can do that, then you can have type imprints and it'll actually, and the builder syntax is not really all that bad. It's actually basically the same. But you'll still have action functions and you'll still have reducer functions. It'll look really, really similar.
 
 export function createWorkflow<TContext extends JsonObject = {}>() {
   function addSteps<T extends JsonObject>(steps: StepBlock<any, any>[]) {
@@ -34,7 +36,7 @@ export function createWorkflow<TContext extends JsonObject = {}>() {
         const newStep: StepBlock<T, TOutput> = {
           title,
           action,
-          reduce
+          reduce,
         };
         const newSteps = [...steps, newStep];
         type NewContext = Simplify<Merge<T, TOutput>>;
