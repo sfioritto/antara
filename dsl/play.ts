8612f4646443b7cbbdc9e3c4e71f1f38121f146b
Sean Fioritto <sean@planningforaliens.com>
2025-01-23 12:46:21 -0600

    Commiting so i can see diffs

--- dsl/play.ts
+++ dsl/play.ts
@@ -1,79 +1,138 @@
-import { JsonObject } from "./types";
+// Represents a workflow step function that transforms the context
+type StepFunction<TContextIn, TContextOut> = (context: TContextIn) => TContextOut;
 
-type Context = JsonObject;
-
-type Action<ActionResult> = () => ActionResult;
-
-type Reducer<
-  ActionResult,
-  ContextIn extends Context,
-  ContextOut extends Context> = (result: ActionResult, context: ContextIn) => ContextOut;
-
-type Builder<ContextIn extends Context> = {
-  step<ContextOut extends Context, ActionResult = any>(
-    title: string,
-    action: Action<ActionResult>,
-    reduce: Reducer<ActionResult, ContextIn, ContextOut>,
-  ): ExtendedBuilder<ContextOut,  FileExtensionReturn<ContextOut> & LoggerExtensionReturn<ContextOut>>,
-  run(): void,
+// Base interface for extensions to implement
+interface WorkflowExtension<TBuilder extends WorkflowBuilder<any>> {
+  // Extensions receive the builder instance and return methods to add
+  extend(builder: TBuilder): Record<string, (...args: any[]) => TBuilder>;
 }
 
-type ExtendedBuilder<
-  ContextIn extends Context,
-  TExtensionsBlock,
-> = TExtensionsBlock & Builder<ContextIn>
+// The builder class needs to be generic over its context
+class WorkflowBuilder<TContext> {
+  private steps: Array<{
+    name: string;
+    fn: StepFunction<any, any>;
+  }> = [];
 
-interface StepBlock<
-  ActionResult,
-  ContextIn extends Context,
-  ContextOut extends Context
-> {
-  title: string,
-  action: Action<ActionResult>,
-  reduce: Reducer<ActionResult, ContextIn, ContextOut>,
-}
-
-type FileExtensionReturn<ContextIn extends Context> = ReturnType<typeof fileExtension<ContextIn>>;
-type LoggerExtensionReturn<ContextIn extends Context> = ReturnType<typeof loggerExtension<ContextIn>>;
-
-function fileExtension<ContextIn extends Context> (
-  builder: Builder<ContextIn>
-) {
-  return {
-    file: {
-      write() {
-        return builder.step(
-          "file step", () => console.log("file action"),
-          (result, context) => {
-            console.log('context in file', context)
-            return {
-              ...context,
-              file: "file content",
-            };
-          }
-        )
-      }
-    },
-  };
-}
-
-function loggerExtension<ContextIn extends Context>(
-  builder: Builder<ContextIn>
-) {
-  return {
-    log() {
-      return builder.step(
-        "Log step", () => console.log("logging action"),
-        (result: any, context) => {
-          return {
-            ...context,
-            logger: "log step",
-          }
-        }
-      );
+  constructor(private extensions: WorkflowExtension<WorkflowBuilder<any>>[]) {
+    // Apply extensions
+    for (const extension of extensions) {
+      const methods = extension.extend(this);
+      Object.assign(this, methods);
     }
-  };
+  }
+
+  // Step method needs to handle type transformation
+  step<TNewContext>(
+    name: string,
+    fn: StepFunction<TContext, TNewContext>
+  ): WorkflowBuilder<TNewContext> {
+    this.steps.push({ name, fn });
+    // Create new builder with updated context type
+    return new WorkflowBuilder<TNewContext>(this.extensions);
+  }
+
+  done(): TContext {
+    // Execute workflow and return final context
+    let context = {} as TContext;
+    for (const step of this.steps) {
+      context = step.fn(context);
+    }
+    return context;
+  }
 }
+
+// Example extension that adds methods and updates context type
+interface WithAddOne {
+  addedValue: number;
+}
+
+const addOneExtension: WorkflowExtension<WorkflowBuilder<any>> = {
+  extend(builder) {
+    return {
+      // The method must return a new builder with updated context type
+      addOne<T>(): WorkflowBuilder<T & WithAddOne> {
+        return builder.step('addOne', (context: T) => ({
+          ...context,
+          addedValue: (context as any).addedValue || 0 + 1
+        }));
+      }
+    };
+  }
+};
+
+function createWorkflow<T>(
+  extensions: WorkflowExtension<WorkflowBuilder<T>>[] = []
+): WorkflowBuilder<T> {
+  return new WorkflowBuilder<T>(extensions);
+}
+
+interface InitialContext {
+  count: number;
+}
+
+const workflow = createWorkflow([addOneExtension]);
+
+const result = workflow
+  .step<InitialContext>('first', () => ({ count: 1 }))
+  // TypeScript knows context has { count: number }
+  .step('second', (context) => ({ ...context, count: context.count + 1 }))
+  // TypeScript knows about addOne from extension
+  .addOne()
+  .done();
+
+
+// interface StepBlock<
+//   ActionResult,
+//   ContextIn extends Context,
+//   ContextOut extends Context
+// > {
+//   title: string,
+//   action: Action<ActionResult>,
+//   reduce: Reducer<ActionResult, ContextIn, ContextOut>,
+// }
+
+// type FileExtensionReturn<ContextIn extends Context> = ReturnType<typeof fileExtension<ContextIn>>;
+// type LoggerExtensionReturn<ContextIn extends Context> = ReturnType<typeof loggerExtension<ContextIn>>;
+
+// function fileExtension<ContextIn extends Context> (
+//   builder: Builder<ContextIn>
+// ) {
+//   return {
+//     file: {
+//       write() {
+//         return builder.step(
+//           "file step", () => console.log("file action"),
+//           (result, context) => {
+//             console.log('context in file', context)
+//             return {
+//               ...context,
+//               file: "file content",
+//             };
+//           }
+//         )
+//       }
+//     },
+//   };
+// }
+
+// function loggerExtension<ContextIn extends Context>(
+//   builder: Builder<ContextIn>
+// ) {
+//   return {
+//     log() {
+//       return builder.step(
+//         "Log step", () => console.log("logging action"),
+//         (result: any, context) => {
+//           return {
+//             ...context,
+//             logger: "log step",
+//           }
+//         }
+//       );
+//     }
+//   };
+// }
 
 // const extensions = [fileExtension, loggerExtension] as const;
 
@@ -102,62 +161,62 @@ function loggerExtension<ContextIn extends Context>(
 // type Original = FileExtensionReturn<{ file: string }> & LoggerExtensionReturn<{ file: string }>;
 // type NEW = ExtensionReturn<{file: string}>
 
-type Extension = <ContextIn extends Context>(builder: Builder<ContextIn>) => {[key: string]: any;}
+// type Extension = <ContextIn extends Context>(builder: Builder<ContextIn>) => {[key: string]: any;}
 
-function createExtensions<ContextIn extends Context>(
-  builder: Builder<ContextIn>,
-  extensions: Extension[],
-) {
-  return extensions.reduce((acc, extension) => ({
-    ...acc,
-    ...extension<ContextIn>(builder)
-  }), {} as any);
-}
+// function createExtensions<ContextIn extends Context>(
+//   builder: Builder<ContextIn>,
+//   extensions: Extension[],
+// ) {
+//   return extensions.reduce((acc, extension) => ({
+//     ...acc,
+//     ...extension<ContextIn>(builder)
+//   }), {} as any);
+// }
 
-function createWorkflow<
-  ContextIn extends Context,
->(
-  options: {
-    steps?: StepBlock<Action<any>, Context, Context>[];
-    extensions?: Extension[];
-  } = {}
-): ExtendedBuilder<ContextIn, FileExtensionReturn<ContextIn> & LoggerExtensionReturn<ContextIn>> {
-  const { steps = [], extensions = [] } = options;
+// function createWorkflow<
+//   ContextIn extends Context,
+// >(
+//   options: {
+//     steps?: StepBlock<Action<any>, Context, Context>[];
+//     extensions?: Extension[];
+//   } = {}
+// ): ExtendedBuilder<ContextIn, FileExtensionReturn<ContextIn> & LoggerExtensionReturn<ContextIn>> {
+//   const { steps = [], extensions = [] } = options;
 
-  const builder: Builder<ContextIn> = {
-    step(title: string, action, reduce) {
-      const stepBlock = {
-        title,
-        action,
-        reduce
-      };
-      type ContextOut = ReturnType<typeof reduce>;
-      return createWorkflow<ContextOut>({
-        steps: [...steps, stepBlock] as StepBlock<Action<any>, Context, Context>[],
-        extensions
-      });
-    },
-    run() {
-      let context = {};
-      for (const { title, action, reduce } of steps) {
-        const result = action();
-        context = reduce(result, context);
-        console.log(JSON.stringify(context, null, 2));
-      }
-    }
-  };
+  // const builder: Builder<ContextIn> = {
+  //   step(title: string, action, reduce) {
+  //     const stepBlock = {
+  //       title,
+  //       action,
+  //       reduce
+  //     };
+  //     type ContextOut = ReturnType<typeof reduce>;
+  //     return createWorkflow<ContextOut>({
+  //       steps: [...steps, stepBlock] as StepBlock<Action<any>, Context, Context>[],
+  //       extensions
+  //     });
+  //   },
+  //   run() {
+  //     let context = {};
+  //     for (const { title, action, reduce } of steps) {
+  //       const result = action();
+  //       context = reduce(result, context);
+  //       console.log(JSON.stringify(context, null, 2));
+  //     }
+  //   }
+  // };
 
-  return {
-    ...builder,
-    ...createExtensions(builder, extensions)
-  }
-}
+//   return {
+//     ...builder,
+//     ...createExtensions(builder, extensions)
+//   }
+// }
 
-const workflow = createWorkflow({ extensions: [fileExtension, loggerExtension] });
-workflow
-  .log()
-  .file.write()
-  .step('first', () => 'first step action', (result, context) => ({ ...context, step1: result + context.logger }))
-  .step('second', () => 'second step action', (result, context) => ({ ...context, step2: result + context.step1 }))
-  .step('third', () => 'third step action', (result, context) => ({ ...context, step3: result + context.step2 }))
-  .run();
+// const workflow = createWorkflow({ extensions: [fileExtension, loggerExtension] });
+// workflow
+//   .log()
+//   .file.write()
+//   .step('first', () => 'first step action', (result, context) => ({ ...context, step1: result + context.logger }))
+//   .step('second', () => 'second step action', (result, context) => ({ ...context, step2: result + context.step1 }))
+//   .step('third', () => 'third step action', (result, context) => ({ ...context, step3: result + context.step2 }))
+//   .run();
