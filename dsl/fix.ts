df66969539791227052791a528d636f122e5079c
Sean Fioritto <sean@planningforaliens.com>
2025-01-24 15:29:40 -0600

    simple extend pattern

--- dsl/fix.ts
+++ dsl/fix.ts
@@ -1,29 +1,32 @@
 import { JsonObject } from "./types";
 
-class Builder {
-  extend(extension: any) {
-    Object.assign(this, extension)
-    return this;
-  }
-  step() {
-    console.log('step')
-    return this;
-  }
+// Define the shape of what will be added when extended
+interface ExtendedMethods {
+    extended(): void;
 }
 
-const simpleExtension: any = {
-  method() {
-    console.log('method')
-    return this.step();
-  }
+// Base class with the extend functionality
+class Extendable {
+    // The extend method returns 'this & ExtendedMethods' to indicate
+    // the return type is the original object plus the new methods
+    extend(extended: any): this & ExtendedMethods {
+        // Add the new method to the instance
+      (this as any).extended = extended;
+
+      return this as this & ExtendedMethods;
+    }
 }
 
-const secondExtension: any = {
-  second() {
-    console.log('second');
-    return this.step();
-  }
-}
+const extendedMethod = () => console.log('extended');
+// Example usage with type checking
+const obj = new Extendable();
 
-const builder = new Builder() as any;
-builder.extend(simpleExtension).extend(secondExtension).step().method().second();
+// This would cause a TypeScript error because 'extended' doesn't exist yet
+// obj.extended();  // Error: Property 'extended' does not exist
+
+// After calling extend(), TypeScript knows the method exists
+const extended = obj.extend(extendedMethod)
+extended.extended();  // Works fine
+
+// You can also chain it
+new Extendable().extend(extendedMethod).extended();
