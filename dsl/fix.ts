d9d8f47c1d79e7ff1133cf0f851ead02b18f284c
Sean Fioritto <sean@planningforaliens.com>
2025-01-24 11:14:50 -0600

    Put some types in place.

--- dsl/fix.ts
+++ dsl/fix.ts
@@ -2,10 +2,18 @@ import { JsonObject } from "./types"
 
 type Builder<TExtensionRecord> = {
   step: () => Builder<TExtensionRecord>
-}
-type Extension<TExtensionRecord> = (builder: Builder<TExtensionRecord>) => object
+};
 
-function createBuilder(...extensions: any[]) {
+type Extension<TExtensionRecord> = (builder: Builder<TExtensionRecord>) => any;
+
+type StepFunction<TExtensionRecord> = () => Builder<TExtensionRecord>;
+
+type ExtensionRecord<TExtensionRecord extends ExtensionRecord<any>> = {
+  [name: string]: StepFunction<TExtensionRecord> | object | ExtensionRecord<TExtensionRecord>
+}
+
+function createBuilder(...extensions: Extension<any>[]) {
+  type ExtensionRecord = ReturnType<typeof extensions[number]>
   const builder = {
     step: () => {
       console.log('base step')
@@ -16,15 +24,17 @@ function createBuilder(...extensions: any[]) {
   return extensions.reduce(
     (acc, ext) => ({
       ...acc,
-      ...ext(builder)
+      ...(ext as Extension<ExtensionRecord>)(builder)
     }),
     builder
-  );
+  ) as Builder<ExtensionRecord>;
 }
 
 const createExtension = <
-  TExtensionRecord, T extends Extension<TExtensionRecord>
->(fn: T): T => fn;
+  TExtensionRecord extends ExtensionRecord<any>, T extends (builder: Builder<TExtensionRecord>) => ExtensionRecord<TExtensionRecord>
+>(fn: T): Extension<TExtensionRecord> => {
+  return (builder: Builder<TExtensionRecord>): ExtensionRecord<any> => fn(builder);
+};
 
 const oneExtension = createExtension((builder) => ({
   one: () => {
@@ -40,6 +50,6 @@ const twoExtension = createExtension((builder) => ({
   }
 }));
 
-// Usage
+const extensions = [oneExtension, twoExtension]
+type TExtensionRecord = ReturnType<typeof extensions[number]>
 const builder = createBuilder(oneExtension, twoExtension);
-builder.step().one().two().step().one().two()
